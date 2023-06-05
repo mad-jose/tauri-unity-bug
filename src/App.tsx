1421752b93dee92f8db0df190ad1e9042890ef41
maddev.jose <joset@yeswearemad.com>
2023-06-05 19:56:25 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <iframe
+        src="/UnityWebGlTest/index.html"
+        title="Index Page"
+        style={{ width: '100%', height: '100%', border: 'none' }}
+      />
+    </div>
+  );
+}
+
+export default App;
